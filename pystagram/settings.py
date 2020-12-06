56c1c1dbd5534ea8a0f8b72cf2525789b2a51be4
Noaarhk <skarb8277@naver.com>
2020-12-06 16:32:32 +0900

    check

--- pystagram/settings.py
+++ pystagram/settings.py
@@ -123,6 +123,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
 
 MEDIA_URL = '/upload_files/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'uploads')
